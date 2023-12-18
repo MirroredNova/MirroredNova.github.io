76ae4df28cb0335a7fba9aae1e324968d1f55dc3
Nate Wiltzius <novawiltzius@gmail.com>
2023-12-18 00:00:48 -0600

    Fixed experiences, added description text

--- src/components/Contact/ContactCard.tsx
+++ src/components/Contact/ContactCard.tsx
@@ -9,6 +9,11 @@ const ContactCard = () => (
       Feel free to reach out to me with any business inquiries, or take some
       time to browse my social media links and public projects.
     </p>
+    <p className="text-default-500">
+      This website serves as a condensed version of my resume. For the latest
+      and most comprehensive information, please refer to my official resume. It
+      provides a detailed overview of my skills, experiences, and achievements.
+    </p>
     {contacts.map((contact, index) => (
       <ContactChunk key={index} contact={contact} />
     ))}
--- src/components/DescriptionCard.tsx
+++ src/components/DescriptionCard.tsx
@@ -3,15 +3,28 @@ import React from "react";
 const DescriptionCard = () => (
   <div className="flex flex-col gap-1">
     <h2 className="text-small">About</h2>
-    <p className="text-default-500">
-      Greetings! I&apos;m Nate Wiltzius, a passionate Full Stack Developer with
-      a penchant for crafting dynamic and responsive products from inception to
-      completion. My enthusiasm for technology extends to exploring new tools
-      and methodologies, embodying a perpetual quest for self-improvement.
-      Committed to delivering innovative solutions, I thrive in the
-      ever-evolving landscape of web development, continually expanding my skill
-      set to push the boundaries of what&apos;s possible.
-    </p>
+    <div className="flex flex-col gap-4 text-default-500">
+      <p>
+        Greetings! I&apos;m Nate Wiltzius, a passionate Full Stack Developer
+        with a penchant for crafting dynamic and responsive products from
+        inception to completion. My enthusiasm for technology extends to
+        exploring new tools and methodologies, embodying a perpetual quest for
+        self-improvement. Committed to delivering innovative solutions, I thrive
+        in the ever-evolving landscape of web development, continually expanding
+        my skill set to push the boundaries of what&apos;s possible.
+      </p>
+      <p>
+        Beyond coding, I embrace a global lifestyle, where I find joy in rock
+        climbing, hiking, and immersing myself in the great outdoors. Whether
+        scaling cliffs or trekking through nature, I relish the diverse
+        landscapes our world offers. In quieter moments, I indulge in reading,
+        play video games, and enjoy movies with my cat. Traveling is a huge
+        passion of mine; exploring new destinations fuels my wanderlust, and
+        I&apos;m always eager for exciting adventures. Meeting new people is a
+        delight as I seek fresh views and perspectives, valuing the richness of
+        global experiences.
+      </p>
+    </div>
   </div>
 );
 
